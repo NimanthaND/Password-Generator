8b0d97b6b2a499fc57c679c57a525b38c59b5082
Nimantha Navodya Dissanayake <87615071+NimanthaND@users.noreply.github.com>
2023-05-27 22:31:52 +0530

    Delete src directory

--- src/Characters.jsx
+++
@@ -1,4 +0,0 @@
-export const number = "0123456789";
-export const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-export const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-export const specialCharacters = "!'^+%&/()=?_#$½§{[]}|;:>÷`<.*-@é"
--- src/Messages.jsx
+++
@@ -1,4 +0,0 @@
-/* messages */
-
-export const COPY_SUCCESS = "Your Password has been copied successfully";
-export const ALERT = "You need to Select atleast 1 option";
